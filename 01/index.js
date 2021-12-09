dbcc5d873014662ecc2e8be0bf914526fc821e5e
Alex <alex.y.kuznetsov@gmail.com>
2021-12-09 20:31:42 +0500

    hw-01

--- 01/index.js
+++ 01/index.js
@@ -1,6 +1,13 @@
 function sum(a, b) {
-
-    
+    try {
+        if (typeof a !== 'number' || typeof b  !== 'number') {
+            throw new TypeError('Both arguments must be numbers');
+        }
+        return a + b;
+    }
+    finally {
+        console.log('done');
+    }
 }
 
 module.exports = sum;

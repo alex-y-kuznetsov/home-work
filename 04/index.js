3873ea4e0f49f14f74bde63cd058b7887840fe69
Alex <alex.y.kuznetsov@gmail.com>
2021-12-16 21:03:47 +0500

    hw-04

--- 04/index.js
+++ 04/index.js
@@ -1,5 +1,19 @@
 function rle(source) {
-        
+    let arrStr = Array.from(source);
+    let newArr = [];
+    let repeatCount = 1;
+    for (let i = 0; i < arrStr.length; i++) {
+        if (arrStr[i] !== arrStr[i + 1]) {
+            newArr.push(arrStr[i])
+            if (repeatCount !== 1) {
+                newArr.push(''+repeatCount);
+            }
+            repeatCount = 1;
+        } else {
+            repeatCount++;
+        }
+    }
+    return newArr.join('');
 }
 
 module.exports = rle;

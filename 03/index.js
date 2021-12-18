a116b59b40ed4767abe8ae0272369c5ade355f89
Alex <alex.y.kuznetsov@gmail.com>
2021-12-18 19:09:37 +0500

    hw-03

--- 03/index.js
+++ 03/index.js
@@ -1,5 +1,17 @@
 function toHex(value) {
-  
+    const notation = 16;
+    const skip = 10;
+    const codes = ['a', 'b', 'c', 'd', 'e', 'f'];
+    let base = value; 
+    let remainder = 0;
+    let remainders = [];
+    while (base > notation) {
+        remainder = base % notation;
+        base = Math.trunc(base / notation);
+        remainder >= skip ? remainders.unshift(codes[remainder - skip]) : remainders.unshift(remainder);
+    };
+    remainders.unshift(base);
+    return remainders.join('');
 }
 
 module.exports = toHex;

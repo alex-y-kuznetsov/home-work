05d6c67375e64afe97931011e15b1dc085e00e02
Alex <alex.y.kuznetsov@gmail.com>
2021-12-19 18:33:17 +0500

    hw-05 draft

--- 05/index.js
+++ 05/index.js
@@ -1,5 +1,20 @@
 function rleDecode(source) {
-        
+    let sourceArr = Array.from(source);
+    let newArr = [];
+    sourceArr.forEach((item, index, arr) => {
+        let number = parseInt(item);
+        let nextItem = arr[index + 1];
+        if (!isNaN(number) && isNaN(parseInt(nextItem)) && number !== 0) {
+            for (let i = 0; i < number; i++) {
+                newArr.push(nextItem);
+            }
+        };
+    });
+    if (!sourceArr.find(item => !isNaN(parseInt(item)))) {
+        newArr = sourceArr;
+    };
+    console.log(newArr.join(''));
+    return newArr.join('');
 }
 
 module.exports = rleDecode;

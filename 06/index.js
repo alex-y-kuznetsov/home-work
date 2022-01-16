9a9ed3956f2ce6e253f196bbea1759329f9e05de
Alex <alex.y.kuznetsov@gmail.com>
2022-01-16 14:45:38 +0500

    hw-06 draft

--- 06/index.js
+++ 06/index.js
@@ -1,5 +1,35 @@
 function getResult(actions) {
-    
+    let decodedActions = 0;
+    let isPower = false;
+    let isPowerCheck = false;
+    let passwordAttempts = 0;
+    let currentPasswordAttempts = 0;
+    let clicks = 0;
+
+    for (let j = 0; j < actions.length; j++) {
+        if (actions[j] === 'keystrokes') {
+            passwordAttempts++;
+        };
+    };
+
+    for (let i = 0; i < actions.length; i++) {
+        if (actions[i] === 'power') {
+            isPower = !isPower;
+        };
+
+        if (actions[i] === 'keystrokes' && isPower && (currentPasswordAttempts < passwordAttempts || currentPasswordAttempts === 0)) {
+            currentPasswordAttempts++;
+        };
+
+        if (actions[i] === 'click' && isPower && currentPasswordAttempts === passwordAttempts) {
+            clicks++;
+            if (clicks === 2) {
+                decodedActions++;
+                clicks = 0;
+            }
+        };
+    };
+    return decodedActions;
 }
 
 module.exports = getResult;

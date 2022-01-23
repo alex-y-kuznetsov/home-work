d4d6551f9d7be662fa8f89ffc5c8829e99800831
Alex <alex.y.kuznetsov@gmail.com>
2022-01-23 17:08:24 +0500

    hw-07

--- 07/index.js
+++ 07/index.js
@@ -1,5 +1,11 @@
 function rgb(r, g, b){
-  
+    function getHex(input) {
+        if (input <= 0) { return '00' };
+        if (input > 0 && input <= 16) { return '0' + input.toString(16) };
+        if (input > 255) { return 'FF' };
+        return input.toString(16);
+    }
+    return getHex(r).toUpperCase() + getHex(g).toUpperCase() + getHex(b).toUpperCase();
 }
 
 

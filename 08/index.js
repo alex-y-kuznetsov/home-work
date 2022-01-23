d29f3b6beb66b745a8a73bfa951b587a44a74786
Alex <alex.y.kuznetsov@gmail.com>
2022-01-23 19:56:31 +0500

    hw-08

--- 08/index.js
+++ 08/index.js
@@ -1,5 +1,16 @@
 function sum(a, b){
-  
+    let sum = (+a + +b);
+    function addNotSafe(a, b) {
+        let arrA = Array.from(a);
+        let arrB = Array.from(b);
+        let result = [];
+        for (let i = 0; i < arrA.length; i++) {
+            result.push(+arrA[i] + +arrB[i]);
+        }
+        return result.join('');
+    };
+
+    return Number.isSafeInteger(sum) ? sum.toString() : addNotSafe(a, b)
 }
 
 module.exports = sum;

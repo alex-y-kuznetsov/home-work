6cb822cfc69af44bf84a90d20c896d232a4bba2b
Alex <alex.y.kuznetsov@gmail.com>
2022-02-13 17:57:45 +0500

    hw-08

--- 08/index.js
+++ 08/index.js
@@ -1,16 +1,32 @@
 function sum(a, b){
     let sum = (+a + +b);
-    function addNotSafe(a, b) {
-        let arrA = Array.from(a);
-        let arrB = Array.from(b);
-        let result = [];
-        for (let i = 0; i < arrA.length; i++) {
-            result.push(+arrA[i] + +arrB[i]);
+
+    function addNotSafe(bigA, bigB) {
+        const aLength = bigA.length;
+        const bLength = bigB.length;
+        const maxLength = Math.max(aLength, bLength);
+
+        let carry = 0; 
+        let temp = 0;
+        let sum = '';
+
+        // складываем посимвольно, если получаем больше 10, записываем первый символ в carry, последний в temp
+        for (let i = 1; i <= maxLength; i++) {
+            let aChar = +bigA.charAt(aLength - i);
+            let bChar = +bigB.charAt(bLength - i);
+
+            temp = carry + aChar + bChar;
+            carry = Math.floor(temp / 10);
+            temp = temp % 10;
+
+            // в последней итерации прибавляем остаток, если он есть, иначе записывем temp, затем конкатенируем с sum
+            sum = (i === maxLength && carry) ? ('' + (carry * 10 + temp)) + sum : ('' + temp) + sum;
         }
-        return result.join('');
+
+        return sum;
     };
 
-    return Number.isSafeInteger(sum) ? sum.toString() : addNotSafe(a, b)
+    return Number.isSafeInteger(sum) ? sum.toString() : addNotSafe(a, b);
 }
 
 module.exports = sum;
